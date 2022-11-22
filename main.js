96680a29f075a834f7578e67fcbb57947a3558de
LucasJanuario <113551206+LucasJanuario@users.noreply.github.com>
2022-11-22 15:34:34 -0300

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ createCard(
     "terça",
     createGame("argentina", "1 x 2", "saudi arabia") +
       createGame("denmark", "0 x 0", "tunisia") +
-      createGame("mexico", "13:00", "poland") +
+      createGame("mexico", "0 x 0", "poland") +
       createGame("france", "16:00", "australia")
   ) +
 
